134b74b300529ed2c023e070878a381bb2671a22
rickdaryll <nandha.ec13@bitsathy.ac.in>
2016-07-28 11:45:50 +0530

    Create oe.c

---
+++ oe.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n; 
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("even number");
+}
+if(n%2==1)
+{
+printf("odd number");
+}
+return 0;
+}
